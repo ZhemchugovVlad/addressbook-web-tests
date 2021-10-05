e54df86b79669aa872e98acb05d9cec6f3c42c93
Владислав Жемчугов <vladislav.zhemchugov@rambler.ru>
2021-10-05 16:55:45 +0300

    Add selector

---
+++ AccountData.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WebAdressbookTests
+{
+    class AccountData
+    {
+        private string username;
+        private string password;
+
+        public AccountData(string username, string password) 
+        {
+            this.username = username;
+            this.password = password;
+        }
+
+        public string Username
+        {
+            get { return username; }
+            set { username = value; }
+        }
+        public string Password
+        {
+            get { return password; }
+            set { password = value; }
+        }
+    }
+
+}
